24512ee14936c6b3954f3bf15845a5fef285f9c3
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2025-11-20 09:26:43 -0300

    refact: adjustment  to history card receive parameters

--- src/components/ui/cards/historycard/index.tsx
+++ src/components/ui/cards/historycard/index.tsx
@@ -5,21 +5,40 @@ import { HStack } from "@/components/base/hstack";
 import { Text } from "@/components/base/text";
 import { VStack } from "@/components/base/vstack";
 
-export function HistoryCard() {
+export type History = {
+  id: number;
+  user_id: number;
+  exercise_id: number;
+  name: string;
+  group: string;
+  created_at: string;
+  hour: string;
+};
+
+type Props = {
+  data: History;
+};
+
+export function HistoryCard({ data }: Props) {
+  const { name, group, hour } = data;
+
   return (
     <Card variant="filled" className="p-4 bg-primary-300 ">
       <HStack className="justify-between items-center">
         <VStack>
-          <Heading size="sm" className="mb-1 font-roboto text-typography-white">
-            Costas
+          <Heading
+            size="sm"
+            className="mb-1 font-roboto text-typography-white capitalize"
+          >
+            {group}
           </Heading>
           <Text size="lg" className="mb-1  font-roboto  text-typography-white">
-            Puxada frontal
+            {name}
           </Text>
         </VStack>
         <Box className="mr-1">
           <Text size="sm" className="mb-1  font-roboto text-typography-150">
-            08:56
+            {hour}
           </Text>
         </Box>
       </HStack>
