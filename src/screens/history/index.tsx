e3181e643cd3b53f8f57552e52b64da96cc640a3
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2025-11-20 09:30:15 -0300

    feat: add integration api on History route

--- src/screens/history/index.tsx
+++ src/screens/history/index.tsx
@@ -1,35 +1,41 @@
 import { Box } from "@/components/base/box";
 import { Heading } from "@/components/base/heading";
 import { HistoryCard } from "@/components/ui/cards/historycard";
+import { HistoryDTO, getHistory } from "@/services/histories";
+import { useFocusEffect } from "@react-navigation/native";
+import { useCallback, useEffect, useState } from "react";
 import { SectionList } from "react-native";
 
-const sections = [
-  {
-    data: ["asdsad"],
-  },
-  {
-    data: ["asdsad"],
-  },
-  {
-    data: ["asdsad", "as233dsad", "asds321ad", "asdsad23", "asd534sad"],
-  },
-];
-
 export function History() {
+  const [histories, setHistories] = useState<HistoryDTO[]>([]);
+
+  const fetchHistories = async () => {
+    try {
+      const { data } = await getHistory();
+      setHistories(data);
+    } catch (e) {}
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchHistories();
+    }, []) // Array de dependências vazio
+  );
+
   return (
     <Box className="flex-1 p-8">
       <SectionList
-        sections={sections}
-        keyExtractor={(item) => item}
-        renderItem={() => (
+        sections={histories}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
           <Box className="mb-3">
-            <HistoryCard />
+            <HistoryCard data={item} />
           </Box>
         )}
-        renderSectionHeader={() => (
+        renderSectionHeader={({ section }) => (
           <Box className="mb-3 mt-10">
             <Heading size="sm" className="font-roboto text-typography-100">
-              26.08.2022
+              {section.title}
             </Heading>
           </Box>
         )}
