eaacf025e9460b5cd88f7e004fa699f7ed91a65a
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2025-11-23 12:12:14 -0300

    refact: remove unncessary typings

--- App.tsx
+++ App.tsx
@@ -4,15 +4,6 @@ import { Routes } from "@/routes/index.route";
 import { Providers } from "@/store";
 import { SystemBars } from "react-native-edge-to-edge";
 
-type Style = "auto" | "inverted" | "light" | "dark";
-
-type SystemBarsProps = {
-  // set the color of the system bar content (as no effect on semi-opaque navigation bar)
-  style?: Style | { statusBar?: Style; navigationBar?: Style };
-  // hide system bars (the navigation bar cannot be hidden on iOS)
-  hidden?: boolean | { statusBar?: boolean; navigationBar?: boolean };
-};
-
 export default function App() {
   return (
     <Providers>
